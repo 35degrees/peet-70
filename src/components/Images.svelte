f4a1d5985405ce51fe74c9c935280769b75f6516
Sam <samhooker@gmail.com>
2024-12-06 14:32:18 -0800

    update

--- src/components/Images.svelte
+++ src/components/Images.svelte
@@ -16,44 +16,44 @@
     <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/1.png" alt="">
   </div>
   <div class="party-img party-img-2" onclick={() => handleClick(2)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/2.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/2.png" alt="">
   </div>
   <div class="party-img party-img-3" onclick={() => handleClick(3)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/3.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/3.png" alt="">
   </div>
   <div class="party-img party-img-4" onclick={() => handleClick(4)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/4.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/4.png" alt="">
   </div>
   <div class="party-img party-img-9 row-span-2 contain h-auto" onclick={() => handleClick(9)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/9.jpeg" alt="" class="h-[96%]">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/9.png" alt="" class="h-[96%]">
   </div>
   <div class="party-img party-img-6" onclick={() => handleClick(6)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/6.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/6.png" alt="">
   </div>
   <div class="party-img party-img-7" onclick={() => handleClick(7)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/7.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/7.png" alt="">
   </div>
   <div class="party-img party-img-8" onclick={() => handleClick(8)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/8.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/8.png" alt="">
   </div>
   <div class="party-img party-img-5" onclick={() => handleClick(5)}>
-    <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/5.jpeg" alt="">
+    <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/5.png" alt="">
   </div>
   <div class="party-img party-img-10 row-span-2" onclick={() => handleClick(10)}>
-     <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/10.jpeg" alt="">
+     <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/10.png" alt="">
   </div>
   <div class="party-img party-img-11" onclick={() => handleClick(11)}>
-     <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/11.jpeg" alt="">
+     <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/11.png" alt="">
   </div>
 
   <div class="party-img party-img-14 row-span-2" onclick={() => handleClick(14)}>
-     <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/14.jpeg" alt="">
+     <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/14.png" alt="">
   </div>
   <div class="party-img party-img-13" onclick={() => handleClick(13)}>
-     <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/13.jpeg" alt="">
+     <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/13.png" alt="">
   </div>
   <div class="party-img party-img-12" onclick={() => handleClick(12)}>
-     <img src="https://raw.githubusercontent.com/35degrees/hhp-70/refs/heads/main/static/images/12.jpeg" alt="">
+     <img src="https://raw.githubusercontent.com/35degrees/peet-70/refs/heads/main/src/lib/images/12.png" alt="">
   </div>
 </div>
 <Modal bind:isModal bind:slide bind:slideArr/>
